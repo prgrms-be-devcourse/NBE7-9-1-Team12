26bcd2a40cf542ecc6e545f3f15e38c810664603
premierbell <pjw319@naver.com>
2025-09-23 17:06:58 +0900

    feat 001 customer 엔티티 추가

--- src/main/java/com/mysite/cuffee/order/entity/Customer.java
+++ src/main/java/com/mysite/cuffee/order/entity/Customer.java
@@ -1,4 +1,24 @@
 package com.mysite.cuffee.order.entity;
 
-public class Customer {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Customer{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+
+    @Column(unique = true)
+    private String email;
+
+    private String address;
+
+    private String zipcode;
 }
