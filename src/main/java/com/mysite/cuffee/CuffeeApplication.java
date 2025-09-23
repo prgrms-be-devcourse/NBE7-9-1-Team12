7693b6a866ce9160fb5f6ad7ed82d35b3403a9ca
Yejin <dpwls8972@gmail.com>
2025-09-23 15:45:12 +0900

    init

---
+++ src/main/java/com/mysite/cuffee/CuffeeApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.cuffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite/cuffee/CuffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.cuffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
