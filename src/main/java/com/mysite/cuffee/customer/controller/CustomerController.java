dbd86c54e8d754ebeabd7a42c6cd61775d7e854c
premierbell <pjw319@naver.com>
2025-09-30 00:46:00 +0900

    feat 001 customerDTO 사용

--- src/main/java/com/mysite/cuffee/customer/controller/CustomerController.java
+++ src/main/java/com/mysite/cuffee/customer/controller/CustomerController.java
@@ -1,9 +1,9 @@
 package com.mysite.cuffee.customer.controller;
 
+import com.mysite.cuffee.customer.dto.customerDto;
 import com.mysite.cuffee.customer.service.CustomerService;
 import com.mysite.global.rsData.RsData;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,22 +16,16 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    record PaymentRequest(
-            @NotBlank @Email String customerEmail,
-            @NotBlank String address,
-            @NotBlank @Size(min = 5, max = 5) String zipcode
-    ) {}
-
     @PostMapping("/carts/{cartId}/customer")
     public RsData<Void> createCustomer(
             @PathVariable Long cartId,
-            @Valid @RequestBody PaymentRequest rqBody){
+            @Valid @RequestBody customerDto rqBody){
 
         customerService.setCartCustomer(
                 cartId,
-                rqBody.customerEmail,
-                rqBody.address,
-                rqBody.zipcode
+                rqBody.customerEmail(),
+                rqBody.address(),
+                rqBody.zipcode()
         );
 
         return new RsData<>(
