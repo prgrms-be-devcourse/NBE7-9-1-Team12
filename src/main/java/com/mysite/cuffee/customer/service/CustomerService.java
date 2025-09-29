9142a7d7cd92a7e1a5ede2ff2290db42f8122f41
premierbell <pjw319@naver.com>
2025-09-30 00:37:39 +0900

    feat 001 customerController역할 분리

--- src/main/java/com/mysite/cuffee/customer/service/CustomerService.java
+++ src/main/java/com/mysite/cuffee/customer/service/CustomerService.java
@@ -1,21 +1,34 @@
 package com.mysite.cuffee.customer.service;
 
+import com.mysite.cuffee.cart.entity.Cart;
 import com.mysite.cuffee.cart.repository.CartRepository;
 import com.mysite.cuffee.customer.entity.Customer;
 import com.mysite.cuffee.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CartRepository cartRepository;
 
     public Customer findOrCreateCustomer(String customerEmail, String address, String zipcode) {
         return customerRepository.findByEmailAndAddressAndZipcode(customerEmail, address, zipcode)
                 .orElseGet(() -> customerRepository.save(new Customer(customerEmail, address, zipcode)));
+    }
+
+    public void setCartCustomer(Long cartId, String customerEmail, String address, String zipcode) {
+        Customer customer = findOrCreateCustomer(customerEmail, address, zipcode);
+        Cart cart = cartRepository.findById(cartId)
+                .orElseThrow(() -> new IllegalArgumentException("장바구니가 존재하지 않습니다. ID: " + cartId));
+
+        cart.setCustomer(customer);
+        cartRepository.save(cart);
     }
 
 }
